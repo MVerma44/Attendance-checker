c224142fe1aa2ffb1f3078308c6781d84868ddcf
Mayank Verma <74061800+MVerma44@users.noreply.github.com>
2023-02-18 18:26:01 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,7 @@
 # python3.9 manage.py collectstatic  --noinput --clear
 # echo " BUILD END"
 
+echo " BUILD START"
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+echo " BUILD END"
